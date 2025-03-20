7a8170eb0889d06397986dade4c6e6247fc633ff
unknown <cloudbishal.kc@gmail.com>
2025-03-20 16:25:31 +0545

    feat: add table

--- src/pages/Sheets/index.tsx
+++ src/pages/Sheets/index.tsx
@@ -1,5 +1,36 @@
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
+
+
 export default function Sheets() {
   return (
-    <div>SpreadSheets</div>
+    <div className="container mx-auto">
+      <Table>
+        <TableCaption>A list of your recent invoices.</TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="w-[100px]">S.N.</TableHead>
+            <TableHead>Name</TableHead>
+            <TableHead>Date</TableHead>
+            <TableHead className="text-right">Modified</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          <TableRow>
+            <TableCell className="font-medium">INV001</TableCell>
+            <TableCell>Paid</TableCell>
+            <TableCell>Credit Card</TableCell>
+            <TableCell className="text-right">$250.00</TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
+    </div>
   );
 }
